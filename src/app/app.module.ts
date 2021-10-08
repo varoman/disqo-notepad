3133b83e55d592a1719b4c831e733a2020c60eda
Varazdat Manukyan <varazdat.manukyan@click2sure.co.za>
2021-10-08 21:53:06 +0400

    Structure skeleton
    Create application initial skeleton composed of 3 main components: Notepad, Note, Statistics.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotepadComponent } from './Notepad/notepad/notepad.component';
+import { NoteComponent } from './Note/note/note.component';
+import { StatisticsComponent } from './Statistics/statistics/statistics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotepadComponent,
+    NoteComponent,
+    StatisticsComponent
   ],
   imports: [
     BrowserModule,
